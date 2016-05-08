39e5e9be1b965e6f4bdb9ad5c59b7e39aeeae939
Aidan <achurch@protonmail.ch>
2016-05-08 10:46:55 -0700

    Some refactoring / fleshing out of socket.io events

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ app = express(),
 server = require('http').createServer(app),
 io =  require('socket.io').listen(server),
 port = process.env.PORT || 3000,
-winston = require('winston');
+winston = require('winston'),
+redis = require("redis"),
+client = redis.createClient();
 
 server.listen(port, function(){
   winston.log("Server listening at port %d", port);
@@ -11,12 +13,57 @@ server.listen(port, function(){
 
 app.use(express.static("public"));
 
+app.get("/rooms/new", function(req, res){
+  var roomId = client.incr("roomSeq");
+  res.redirect("/poll.html?"+roomId);
+});
+
+function upvote(roomId, questionId){
+  client.incr(redisKeys.getQuestionVotes(roomId, questionId));
+}
+
+function downvote(roomId, questionId){
+  client.decr(redis.getQuestionVotes(roomId, questionId));
+}
+
+function createNewQuestion(roomId, question){
+  var questionId = client.incr(getQuestionSeq(roomId));
+  client.hset(redisKeys.getRoom(roomId), questionId, question);
+  upvote(roomId, questionId);
+}
+
+function getCurrentState(roomId){
+  var state = {};
+  var questions = client.hgetall(redisKeys.getRoom(roomId));
+  for(var questionId in questions){
+    var votes = client.get(redisKeys.getQuestionVotes(roomId, questionId));
+    state[questionId] = {"question" : questions[questionId], "id": questionId}
+  }
+  return state;
+}
+
 io.on('connection', function(socket){
-  winston.debug("A user has connected");
+
   socket.on('join', function(msg){
     var room = msg.room;
     socket.join(room);
     winston.debug("A user has joined room= " + room);
+    io.to(msg.roomId).emit("update",getCurrentState());
+  });
+
+  socket.on("upvote", function(msg){
+    upvote(msg.roomId, msg.questionId);
+    io.to(msg.roomId).emit("update",getCurrentState());
+  });
+
+  socket.on("downvote", function(msg){
+    downvote(msg.roomId, msg.questionId);
+    io.to(msg.roomId).emit("update",getCurrentState());
+  });
+
+  socket.on('newQuestion', function(msg){
+    createNewQuestion(msg.roomId, msg.question);
+    io.to(msg.roomId).emit("update",getCurrentState());
   });
 
   socket.on('disconnect', function(){
@@ -27,3 +74,18 @@ io.on('connection', function(socket){
     winston.error("An error has occurred ", e);
   });
 });
+
+var redisKeys = {
+  getQuestionVotes: function(roomId, questionId){
+    return "r."+roomId+".q."+questionId+".v";
+  },
+  getRoomSeq: function(){
+    return "r.seq";
+  },
+  getQuestionSeq: function(roomId){
+    return "r."+roomId+".seq";
+  },
+  getRoom: function(roomId){
+    return "r."+roomId;
+  }
+}
